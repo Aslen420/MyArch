4d7127f25352bdc35ee4c03d0dd6aac247e22abc
Aslen420 <kipzydrunas@gmail.com>
2022-04-09 18:31:15 +0000

    Fixed indentation

--- setup.sh
+++ setup.sh
@@ -4,23 +4,23 @@ mount /dev/sda2 /mnt
 pacstrap /mnt base linux linux-firmware nano neofetch
 genfstab -U /mnt >> /mnt/etc/fstab
 cat << EOF | arch-chroot /mnt
-  timedatectl set-timezone Canada/Central
-  locale-gen
-  echo LANG=en_US.UTF-8 > /etc/locale.conf
-  export LANG=en_US.UTF-8
-  echo aslen-pc > /etc/hostname
-  touch /etc/hosts
-  echo "127.0.0.1     localhost" > ~/.test.txt
-  echo "::1           localhost" >> ~/.test.txt
-  echo "127.0.1.1     aslen-pc" >> ~/.test.txt
-  echo "root:test" | chpasswd -R /mnt
-  pacman -S grub efibootmgr
-  mkdir /boot/efi
-  mount /dev/sda1 /boot/efi
-  grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
-  grub-mkconfig -o /boot/grub/grub.cfg
-  useradd -m aslen
-  echo "aslen:test" | chpasswd -R /mnt
+timedatectl set-timezone Canada/Central
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+echo aslen-pc > /etc/hostname
+touch /etc/hosts
+echo "127.0.0.1     localhost" > ~/.test.txt
+echo "::1           localhost" >> ~/.test.txt
+echo "127.0.1.1     aslen-pc" >> ~/.test.txt
+echo "root:test" | chpasswd -R /mnt
+pacman -S grub efibootmgr
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+useradd -m aslen
+echo "aslen:test" | chpasswd -R /mnt
 pacman -S sudo nano
 EDITOR=nano visudo
 sudo pacman -S --needed xorg
