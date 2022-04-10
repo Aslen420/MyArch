81e564f335796337c5c71813f871a2eee33df7a0
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-04-10 00:12:18 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 
-echo -e "o\nn\np\n1\nn\np\n2\n\nw" | fdisk /dev/sda
+echo -e "o\nn\np\n1\n\n\n\nn\np\n2\n\n\nw" | fdisk /dev/sda
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
